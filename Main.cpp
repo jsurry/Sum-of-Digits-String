333208c2382bb4a7df897df6720fc07e668f5cfe
Jesse Surry <jsurry@zagmail.gonzaga.edu>
2019-04-17 11:45:14 -0700

    prompt user for a number

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
    int main () {
+   string numberStr;
+   cout << "Please enter a number " << endl;
+   cin >> numberStr;
+   cout<<"You entered: " << numberStr << endl;
       
    return 0;
    
